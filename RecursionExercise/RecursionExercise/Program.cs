bfbace2494fc024d1ca47e96879bdf641a057ee6
barakbaruch1 <35428276+barakbaruch1@users.noreply.github.com>
2020-08-23 12:45:35 +0300

    Commit

---
+++ RecursionExercise/RecursionExercise/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace RecursionExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Is Palindrome:");
+            Console.WriteLine(Recursion.IsPalindrome("aba"));
+            Console.WriteLine(Recursion.IsPalindrome("qaz"));
+            Console.WriteLine(Recursion.IsPalindrome("a"));
+            Console.WriteLine(Recursion.IsPalindrome("AB"));
+            Console.WriteLine(Recursion.IsPalindrome("ABBA"));
+            Console.WriteLine(Recursion.IsPalindrome("ABBB"));
+
+            Console.WriteLine("Is Prime:");
+   //         Console.WriteLine(IsPrime(6));
+            Console.WriteLine(Recursion.IsPrime(25));
+
+            Console.WriteLine("Sum Digits:");
+            Console.WriteLine(Recursion.SumDigit(25));
+            Console.WriteLine(Recursion.SumDigit(2));
+            Console.WriteLine(Recursion.SumDigit(0));
+            Console.WriteLine(Recursion.SumDigit(101));
+
+            Console.WriteLine("Min In Arr");
+            int[] arr = { 5, 2, 3, 7 };
+            int[] arr1 = {2,2,2,3};
+            Console.WriteLine(Recursion.MinInArr(arr));
+            Console.WriteLine(Recursion.MinInArr(arr1));
+            
+            Console.WriteLine("Duplication:");
+            Console.WriteLine(Recursion.Duplication(arr1, 2));
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ RecursionExercise/RecursionExercise/Recursion.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RecursionExercise
+{
+    public static class Recursion
+    {
+
+        public static bool IsPalindrome(string str)
+        {
+            bool result = false;
+            if (str.Length <= 1) return true;
+            if (str[0] == str[str.Length - 1]) result = true;
+            return IsPalindrome(str.Substring(1, str.Length - 2)) && result;
+        }
+
+
+        public static int Duplication(int[] arr, int num)
+        {
+            return Duplication(arr, num, 0);
+        }
+
+        private static int Duplication(int[] arr,int num, int index)
+        {
+            if (index >= arr.Length) return 0;
+            if (arr[index] == num) return Duplication(arr, num, index + 1) + 1;
+            return Duplication(arr, num, index + 1);
+        }
+
+        public static int MinInArr(int[] arr)
+        {
+            return MinInArr(0, int.MaxValue ,arr);
+        }
+
+        private static int MinInArr(int index,int minNum,int[] arr)
+        {
+            if (index >= arr.Length) return minNum;
+
+            if (arr[index] < minNum) minNum = arr[index];
+
+            return MinInArr(index + 1, minNum, arr);
+        }
+
+        public static int SumDigit(int num)
+        {
+            if(num <= 0) return 0;
+            return SumDigit(num / 10) + num % 10;
+        }
+
+        public static bool IsPrime(int num)
+        {
+            return IsPrime(2, (int)Math.Sqrt(num), num);
+        }
+
+        private static bool IsPrime(int divideNum, int maxdivideNum, int num)
+        {
+            if (num == 1 || num == 0)
+                return false;
+            if (divideNum > maxdivideNum)
+                return true;
+            if (num % divideNum == 0) return false;
+            return IsPrime(divideNum + 1, maxdivideNum, num);
+        }
+
+    }
+}
